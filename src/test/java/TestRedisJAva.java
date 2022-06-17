1675b7b5e4031142e481cc65e1f6309b0ad44ed3
andreimatrosautr <andrei.matrosau@thomsonreuters.com>
2022-06-17 06:46:37 +0000

    test

--- src/test/java/TestRedisJAva.java
+++ src/test/java/TestRedisJAva.java
@@ -3,20 +3,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
-public class TestRedisJava {
-    public TestRedisJava(){
+public class TestDeepThought {
+    public TestDeepThought() {
 
     }
+        @Test
+        @DisplayName("Test the answer to everything")
+        void testAnswerToAll(){
+            DeepThought superComputer = new DeepThought();
+            assertEquals( 42 , superComputer.answer_to_the_ultimate_question_of_life_the_universe_and_everything());
+        }
 
-    @Test
-    @DisplayName("Test Redis cache")
-    void testRedisCache(){
-        DeepThought ourSuperComputer = new DeepThought();
-        int ans = ourSuperComputer.answer_to_the_ultimate_question_of_life_the_universe_and_everything();
-
-        RedisJava cache = new RedisJava();
-        cache.cache_value("answer_to_the_ultimate_question_of_life_the_universe_and_everything",String.valueOf(ans));
-
-        assertEquals("42", cache.get_cached_value("answer_to_the_ultimate_question_of_life_the_universe_and_everything"));
-    }
 }
