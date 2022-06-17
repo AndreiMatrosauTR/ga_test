dac5344eeadda1ace0ec5806c03305fc62b57d8b
andreimatrosautr <andrei.matrosau@thomsonreuters.com>
2022-06-17 10:38:29 +0000

    06/17/2022 10:38:28

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,11 @@
-public class App
-{
-  public static void main( String[] args )
-  {
-    DeepThought ourSuperComputer = new DeepThought();
-    int ans = ourSuperComputer.answer_to_the_ultimate_question_of_life_the_universe_and_everything();
-    System.out.println(ans);
-  }
+package demo;
+
+public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
 }
